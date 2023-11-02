2f32a4ebeaf5e1c258905c7aa892c75072686827
Nathan <kiprotichnathan@yahoo.com>
2023-11-02 10:21:51 +0300

    done

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,8 @@
-import React from "react";
-
 function Article() {
   return (
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
-
-export default Article;
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
